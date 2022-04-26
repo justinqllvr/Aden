498e759f59d9937aa4cce9128632ac199bae71d8
justinqllvr <42967513+justinqllvr@users.noreply.github.com>
2022-04-26 16:52:56 +0200

    first interractable object

---
+++ Assets/Scripts/Interract.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interract : MonoBehaviour
+{
+    public GameObject go = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; 
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if(hit.transform.gameObject.tag == "interractable")
+            {
+                go = hit.transform.gameObject;
+                go.GetComponent<Outline>().enabled = true;
+            }
+            else
+            {
+                if(go != null && go.GetComponent<Outline>())
+                {
+                    go.GetComponent<Outline>().enabled = false;
+                    go = null;
+                }
+            }
+        }
+       
+    }
+}
