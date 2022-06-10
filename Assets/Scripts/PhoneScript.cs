2ab11f20c65318e944c8ee36fce91c5a441ebe1a
Enzo Cailleton <49985085+ZoCailleton@users.noreply.github.com>
2022-06-10 15:20:23 +0200

    UI tuto

--- Assets/Scripts/PhoneScript.cs
+++ Assets/Scripts/PhoneScript.cs
@@ -7,18 +7,42 @@ public class PhoneScript : MonoBehaviour
     public static bool PhoneIsOn = false;
 
     public GameObject phoneUI;
+    public GameObject score1;
+    public GameObject score2;
+    public GameObject options;
+    public GameObject response;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.R)) {
-            Debug.Log("Text: " + PhoneIsOn);
             if(PhoneIsOn) {
                 Resume();
             } else {
                 OpenPhone();
             }
         }
+
+        if(Input.GetKeyDown(KeyCode.A)) {
+            options.SetActive(false);
+            response.SetActive(true);
+            score1.SetActive(false);
+            score2.SetActive(true);
+            StartCoroutine(MyCoroutine());
+        }
+
+        if(Input.GetKeyDown(KeyCode.B)) {
+            options.SetActive(false);
+            response.SetActive(true);
+            score1.SetActive(false);
+            score2.SetActive(true);
+            StartCoroutine(MyCoroutine());
+        }
+    }
+
+    IEnumerator MyCoroutine() {
+        yield return new WaitForSeconds(3);
+        phoneUI.SetActive(false);
     }
 
     void Resume() {
