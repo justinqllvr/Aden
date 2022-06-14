4bf8beb53d19b6c4f20831674fe76cbad24d68bf
Enzo Cailleton <49985085+ZoCailleton@users.noreply.github.com>
2022-06-14 11:18:33 +0200

    UI

--- Assets/Scripts/MenuCamera.cs
+++ Assets/Scripts/MenuCamera.cs
@@ -6,6 +6,7 @@ public class MenuCamera : MonoBehaviour
 {
 
     public int delayCam2;
+    public int delayCam3;
     public Camera[] cameras;
 
     // Start is called before the first frame update
@@ -18,6 +19,13 @@ public class MenuCamera : MonoBehaviour
         yield return new WaitForSeconds(delayCam2);
         cameras[0].gameObject.SetActive(false);
         cameras[1].gameObject.SetActive(true);
+        StartCoroutine(cam3());
+    }
+
+    IEnumerator cam3() {
+        yield return new WaitForSeconds(delayCam3);
+        cameras[1].gameObject.SetActive(false);
+        cameras[2].gameObject.SetActive(true);
     }
 
 }
